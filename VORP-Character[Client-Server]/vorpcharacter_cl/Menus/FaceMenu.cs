072275c65b122d7e192431b451ded9affe2a969a
Osmany González <devbiitz2@gmail.com>
2020-06-14 08:29:37 +0100

    New Menu System 2.0

---
+++ VORP-Character[Client-Server]/vorpcharacter_cl/Menus/FaceMenu.cs
@@ -0,0 +1,15 @@
+﻿using CitizenFX.Core;
+using MenuAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vorpcharacter_cl.Utils;
+
+namespace vorpcharacter_cl.Menus
+{
+    class FaceMenu
+    {
+    }
+}
---
+++ VORP-Character[Client-Server]/vorpcharacter_cl/Menus/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using CitizenFX.Core;
+using MenuAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vorpcharacter_cl.Utils;
+
+namespace vorpcharacter_cl.Menus
+{
+    class MainMenu
+    {
+        private static Menu mainMenu = new Menu(Language.Langs["TitleMenuBody"], Language.Langs["SubTitleMenuBody"]);
+        private static bool setupDone = false;
+        private static void SetupMenu()
+        {
+            if (setupDone) return;
+            setupDone = true;
+            MenuController.AddMenu(mainMenu);
+
+            MenuController.EnableMenuToggleKeyOnController = false;
+            MenuController.MenuToggleKey = (Control)0;
+
+            //Weapons Buy Menu
+            //MenuController.AddSubmenu(mainMenu, BuyMenu.GetMenu());
+
+            //MenuItem subMenuBuyBtn = new MenuItem(GetConfig.Langs["MenuMainButtonBuyWeapons"], " ")
+            //{
+            //    RightIcon = MenuItem.Icon.ARROW_RIGHT
+            //};
+
+            //mainMenu.AddMenuItem(subMenuBuyBtn);
+            //MenuController.BindMenuItem(mainMenu, BuyMenu.GetMenu(), subMenuBuyBtn);
+
+
+
+            mainMenu.OnMenuOpen += (_menu) => {
+                
+            };
+
+            mainMenu.OnMenuClose += (_menu) =>
+            {
+
+            };
+
+            mainMenu.OnItemSelect += (_menu, _item, _index) =>
+            {
+                // Code in here would get executed whenever an item is pressed.
+                if (_index == 3)
+                {
+                    //isDressUpPed = false;
+                    //SaveChanges();
+                    //mdu.CloseMenu();
+                }
+            };
+
+        }
+        public static Menu GetMenu()
+        {
+            SetupMenu();
+            return mainMenu;
+        }
+
+    }
+}
