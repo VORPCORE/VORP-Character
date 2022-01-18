905bef3d2af5f70880c6ee3a55aef4b4daff36a6
local9 <apjcookie1@hotmail.com>
2022-01-18 23:19:15 +0000

    add plugin manager

---
+++ VORP-Character[Client-Server]/vorpcharacter_sv/PluginManager.cs
@@ -0,0 +1,17 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorpCharacter
+{
+    public class PluginManager : BaseScript
+    {
+        public PluginManager()
+        {
+
+        }
+    }
+}
